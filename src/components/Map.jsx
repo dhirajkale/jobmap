21c4ea19084ba3309572aa4556b017b758828c1a
dhirajkale <dhirajkale00001@gmail.com>
2026-02-01 10:08:59 +0530

    style: switch to standard OpenStreetMap tiles

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -52,12 +52,10 @@ const createClusterCustomIcon = (cluster) => {
 export default function Map() {
     const { filteredJobs, mapCenter, mapZoom, theme, jobsLoading } = useStore()
 
-    // Choose tile layer based on theme
-    const tileUrl = theme === 'dark'
-        ? 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
-        : 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
+    // Use OpenStreetMap standard tiles
+    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 
-    const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+    const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 
     return (
         <div className="map-container">
